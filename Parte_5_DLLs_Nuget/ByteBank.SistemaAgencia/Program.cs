e09ff723f3d2bc7cbd025cf521632280501c07cf
Karine Riquena Limp <karine_limp@hotmail.com>
2020-04-27 17:07:46 -0300

    Referencia de DLL em outro projeto, Documentando a Biblioteca e Gerenciamento de pacotes com NuGet

--- Parte_5_DLLs_Nuget/ByteBank.SistemaAgencia/Program.cs
+++ Parte_5_DLLs_Nuget/ByteBank.SistemaAgencia/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ByteBank.Modelos;
 using ByteBank.Modelos.Funcionarios;
+using Humanizer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,22 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(847, 489754);
+            DateTime dataFimPagamento = new DateTime(2020, 5, 2);
+            DateTime dataCorrente = DateTime.Now;
 
-            FuncionarioAutenticavel carlos = null;
-            Console.WriteLine(conta.Numero);
+            TimeSpan diferenca = TimeSpan.FromMinutes(40);// dataFimPagamento - dataCorrente;
+            string mensagem = "Vencimento em: " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(dataCorrente);
+            Console.WriteLine(dataFimPagamento);
+            Console.WriteLine(mensagem);
+            //ContaCorrente conta = new ContaCorrente(847, 489754);
+            //new ContaCorrente(123, 456456);
+
+            //FuncionarioAutenticavel carlos = null;
+            //carlos.Autenticar("kashdajdha");
+
+            //Console.WriteLine(conta.Numero);
 
             Console.ReadLine();
         }
---
+++ Parte_5_DLLs_SistemaInterno/ByteBank.SistemaInterno/ByteBank.SistemaInterno/Program.cs
@@ -0,0 +1,25 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaInterno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(458, 455789);
+            Console.WriteLine(conta.Saldo);
+
+            ContaCorrente conta2 = new ContaCorrente(567, 987654);
+
+            conta.Sacar(-10);
+            string nome = "Guilherme";
+
+            Console.ReadLine();
+        }
+    }
+}

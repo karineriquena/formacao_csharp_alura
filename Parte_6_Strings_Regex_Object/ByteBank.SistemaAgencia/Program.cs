c69eda975d56c798e6c1646b50910138d2f3749a
Karine Riquena Limp <karine_limp@hotmail.com>
2020-05-19 12:22:11 -0300

    Parte 6 - aula 2

---
+++ Parte_6_Strings_Regex_Object/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,36 @@
+﻿using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
+            int indiceInterrogacao = url.IndexOf("?");
+            string argumentos = url.Substring(indiceInterrogacao + 1); // inclusivo: a partir do indice 6, contendo o indice 6
+
+            Console.WriteLine(indiceInterrogacao);
+            Console.WriteLine(url);
+            Console.WriteLine(argumentos);
+
+            string textoVazio = "";
+            string textoNulo = "";
+            string textoQualquer = "sdfisfishdfkfsk";
+
+            Console.WriteLine(String.IsNullOrEmpty(textoVazio));
+            Console.WriteLine(String.IsNullOrEmpty(textoNulo));
+            Console.WriteLine(String.IsNullOrEmpty(textoQualquer));
+
+            // ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL("");
+
+            Console.ReadLine();
+        }
+    }
+}

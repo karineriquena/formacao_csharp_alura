655da045f1e6ae64bfcbb9339bbd679d1b3d01df
Karine Riquena Limp <karine_limp@hotmail.com>
2020-05-20 16:55:21 -0300

    Aula 3 - IndexOf Startswith Endswith e Contains

--- Parte_6_Strings_Regex_Object/ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
+++ Parte_6_Strings_Regex_Object/ByteBank.SistemaAgencia/ExtratorValorDeArgumentosURL.cs
@@ -26,8 +26,21 @@ namespace ByteBank.SistemaAgencia
         // moedaOrigem=real&moedaDestino=dolar
         public string GetValor(string nomeParametro)
         {
-            return "";
-            // int indiceParametro = URL.
+            nomeParametro = nomeParametro.ToUpper();
+            string argumentosEmCaixaAlta = _argumentos.ToUpper();
+
+            string termo = nomeParametro + "=";
+            int indiceTermo = argumentosEmCaixaAlta.IndexOf(termo);
+
+            string resultado = _argumentos.Substring(indiceTermo + termo.Length);
+            int indiceEComercial = resultado.IndexOf("&");
+
+            if (indiceEComercial == -1)
+            {
+                return resultado;
+            }
+
+            return resultado.Remove(indiceEComercial);
         }
     }
 }
--- Parte_6_Strings_Regex_Object/ByteBank.SistemaAgencia/Program.cs
+++ Parte_6_Strings_Regex_Object/ByteBank.SistemaAgencia/Program.cs
@@ -12,7 +12,7 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
+            /*string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
             int indiceInterrogacao = url.IndexOf("?");
             string argumentos = url.Substring(indiceInterrogacao + 1); // inclusivo: a partir do indice 6, contendo o indice 6
 
@@ -26,14 +26,15 @@ namespace ByteBank.SistemaAgencia
             string textoQualquer = "sdfisfishdfkfsk";
             Console.WriteLine(String.IsNullOrEmpty(textoVazio));
             Console.WriteLine(String.IsNullOrEmpty(textoNulo));
-            Console.WriteLine(String.IsNullOrEmpty(textoQualquer));
+            Console.WriteLine(String.IsNullOrEmpty(textoQualquer));*/
 
             // Testando o IndexOf com palavra
             // moedaOrigem=real&moedaDestino=dolar
             //                              |
             //              ----------------'
-            string palavra = "moedaOrigem=real&moedaDestino=dolar";
-            string nomeArgumento = "moedaDestino";
+            // <nome>=<valor>
+            /*string palavra = "moedaOrigem=real&moedaDestino=dolar";
+            string nomeArgumento = "moedaDestino=";
 
             int indice = palavra.IndexOf(nomeArgumento);
             Console.WriteLine(indice);
@@ -41,9 +42,62 @@ namespace ByteBank.SistemaAgencia
             Console.WriteLine("Tamanho da string nomeArgumento: " + nomeArgumento.Length);
             Console.WriteLine(palavra);
             Console.WriteLine(palavra.Substring(indice));
-            Console.WriteLine(palavra.Substring(indice + nomeArgumento.Length + 1));
+            Console.WriteLine(palavra.Substring(indice + nomeArgumento.Length));*/
 
-            // ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL("");
+            // Testando remoção de partes da string
+            //string testeRemocao = "primeiraParte&parteParaRemover";
+            //int indiceEComercial = testeRemocao.IndexOf("&");
+            //Console.WriteLine(testeRemocao.Remove(indiceEComercial, 4));
+            //Console.ReadLine();
+
+            // Testando quando o IndexOf não encontra o caractere na string
+            //string qualquer = "sdfsdfjkgljdgf";
+            //Console.WriteLine(qualquer.IndexOf("1"));
+            //Console.ReadLine();
+
+            // Testando diferenciação de maiuscula e minúscula com replace e upper e lower
+            //string mensagemOrigem = "PALAVRA";
+            //string termoBusca = "ra";
+
+            //Console.WriteLine(termoBusca.ToUpper());
+            //Console.WriteLine(mensagemOrigem.ToLower());
+
+            //termoBusca = termoBusca.Replace("r", "R");
+            //Console.WriteLine(termoBusca);
+
+            //termoBusca = termoBusca.Replace("a", "A");
+            //Console.WriteLine(termoBusca);
+
+            //Console.WriteLine(mensagemOrigem.IndexOf(termoBusca));
+            //Console.ReadLine();
+
+            // Testando Startswith, Endswith e Contains
+            string urlTeste = "https://www.bytebank.com/cambio";
+            int indiceByteBank = urlTeste.IndexOf("https://www.bytebank.com");
+            Console.WriteLine("(IndexOf) É uma URL do ByteBank? " + (indiceByteBank == 0));
+            Console.WriteLine("(StartsWith) É uma URL do ByteBank? " + (urlTeste.StartsWith("https://www.bytebank.com")));
+            Console.WriteLine("(EndsWith) Está na página do cambio? " + (urlTeste.EndsWith("cambio/")));
+            Console.WriteLine("(Contains) Contém uma string? " + (urlTeste.Contains("ByteBank")));
+            Console.ReadLine();
+
+            // Testando extração de números com expressão regular (Regex)
+            // Suponha o texto abaixo:
+            // Olá, meu nome é Guilherme e você pode entrar em contato comigo
+            // pelo número 8457-4456!
+            // Ou esse texto:
+            // Meu nome é Guilherme, me ligue em 4784-4546
+
+
+
+            // Testando classe de parametros
+            string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
+            ExtratorValorDeArgumentosURL extratorDeValores = new ExtratorValorDeArgumentosURL(urlParametros);
+            string valorMoedaOrigem = extratorDeValores.GetValor("moedaOrigem");
+            string valorMoedaDestino = extratorDeValores.GetValor("moedaDestino");
+            string valorConverter = extratorDeValores.GetValor("VALOR");
+            Console.WriteLine("Valor de moedaOrigem: " + valorMoedaOrigem);
+            Console.WriteLine("Valor de moedaDestino: " + valorMoedaDestino);
+            Console.WriteLine("Valor para converter (valor): " + valorConverter);
 
             Console.ReadLine();
         }

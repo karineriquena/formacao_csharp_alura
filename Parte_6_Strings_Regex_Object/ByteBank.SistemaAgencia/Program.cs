41375f8ff941c940c595db0310cef78a32528a92
Karine Riquena Limp <karine_limp@hotmail.com>
2020-06-25 21:18:07 -0300

    Parte 6 - Aula 4 - Regex

--- Parte_6_Strings_Regex_Object/ByteBank.SistemaAgencia/Program.cs
+++ Parte_6_Strings_Regex_Object/ByteBank.SistemaAgencia/Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ByteBank.SistemaAgencia
@@ -12,6 +13,40 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            // Testando extração de números com expressão regular (Regex)
+            // Suponha o texto abaixo:
+
+            // Olá, meu nome é Guilherme e você pode entrar em contato comigo
+            // pelo número 8457-4456!
+
+            // Ou esse texto:
+
+            // Meu nome é Guilherme, me ligue em 4784-4546
+
+            // Expressão regular - 'Linguagem do padrão - tem que estar dentro das regras'
+            // string padrao = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
+            // string padrao = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
+            // string padrao = "[0-9]{4}[-][0-9]{4}"; 
+            // string padrao = "[0-9]{4,5}[-][0-9]{4}"; // supondo que o numero tem o '9' na frente
+            // string padrao = "[0-9]{4,5}[-]{0,1}[0-9]{4}"; // supondo que o numero não tem o hifen
+            // string padrao = "[0-9]{4,5}-{0,1}[0-9]{4}"; // simplificando o de cima pois o grupo só tem 1 caractere no grupo "-"
+            // string padrao = "[0-9]{4,5}-?[0-9]{4}"; // a '?' substitui o quantificador {0,1} "opcional"
+
+            string padrao = "[0-9]{4,5}-?[0-9]{4}";            
+            string textoDeTeste = "Meu nome é Guilherme, me ligue em 94784-4546"; 
+
+            Match resultado = Regex.Match(textoDeTeste, padrao);
+            Console.WriteLine(resultado.Value); // retorna o valor do texto que dá match com a regex
+            
+            // Console.WriteLine(Regex.IsMatch(textoDeTeste, padrao)); // IsMatch retorna um booleano
+
+
+            
+            
+            
+            
+            
+            
             /*string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
             int indiceInterrogacao = url.IndexOf("?");
             string argumentos = url.Substring(indiceInterrogacao + 1); // inclusivo: a partir do indice 6, contendo o indice 6
@@ -72,32 +107,23 @@ namespace ByteBank.SistemaAgencia
             //Console.ReadLine();
 
             // Testando Startswith, Endswith e Contains
-            string urlTeste = "https://www.bytebank.com/cambio";
+            /*string urlTeste = "https://www.bytebank.com/cambio";
             int indiceByteBank = urlTeste.IndexOf("https://www.bytebank.com");
             Console.WriteLine("(IndexOf) É uma URL do ByteBank? " + (indiceByteBank == 0));
             Console.WriteLine("(StartsWith) É uma URL do ByteBank? " + (urlTeste.StartsWith("https://www.bytebank.com")));
             Console.WriteLine("(EndsWith) Está na página do cambio? " + (urlTeste.EndsWith("cambio/")));
             Console.WriteLine("(Contains) Contém uma string? " + (urlTeste.Contains("ByteBank")));
-            Console.ReadLine();
-
-            // Testando extração de números com expressão regular (Regex)
-            // Suponha o texto abaixo:
-            // Olá, meu nome é Guilherme e você pode entrar em contato comigo
-            // pelo número 8457-4456!
-            // Ou esse texto:
-            // Meu nome é Guilherme, me ligue em 4784-4546
-
-
+            Console.ReadLine();*/
 
             // Testando classe de parametros
-            string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
+            /*string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
             ExtratorValorDeArgumentosURL extratorDeValores = new ExtratorValorDeArgumentosURL(urlParametros);
             string valorMoedaOrigem = extratorDeValores.GetValor("moedaOrigem");
             string valorMoedaDestino = extratorDeValores.GetValor("moedaDestino");
             string valorConverter = extratorDeValores.GetValor("VALOR");
             Console.WriteLine("Valor de moedaOrigem: " + valorMoedaOrigem);
             Console.WriteLine("Valor de moedaDestino: " + valorMoedaDestino);
-            Console.WriteLine("Valor para converter (valor): " + valorConverter);
+            Console.WriteLine("Valor para converter (valor): " + valorConverter);*/
 
             Console.ReadLine();
         }

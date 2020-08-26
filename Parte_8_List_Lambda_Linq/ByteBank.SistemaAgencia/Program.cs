c9a88114503294a72f4e17e06a8958525293158a
Karine Riquena Limp <karine_limp@hotmail.com>
2020-08-26 19:24:59 -0300

    Uso do var e Sort em listas

--- Parte_8_List_Lambda_Linq/ByteBank.SistemaAgencia/Program.cs
+++ Parte_8_List_Lambda_Linq/ByteBank.SistemaAgencia/Program.cs
@@ -13,13 +13,37 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            // ListComExtensionMethod();
+            // UsoDoVar();
+            // ListComSort();
+
+            var contas = new List<ContaCorrente>()
+            {
+                new ContaCorrente(341, 57480),
+                new ContaCorrente(342, 45678),
+                new ContaCorrente(340, 48950),
+                new ContaCorrente(290, 18950),
+            };
+
+            contas.Sort();
+
+            foreach (var conta in contas)
+            {
+                Console.WriteLine($"Conta número {conta.Numero}, ag. {conta.Agencia}");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void ListComExtensionMethod()
+        {
             List<int> idades = new List<int>();
 
             idades.Add(1);
             idades.Add(5);
             idades.Add(14);
             idades.Add(25);
-            idades.Add(38); 
+            idades.Add(38);
             idades.Add(61);
 
             // No ADDRANGE não é possivel usar igual o argumento params
@@ -36,8 +60,65 @@ namespace ByteBank.SistemaAgencia
             {
                 Console.WriteLine(idades[i]);
             }
+        }
 
-            Console.ReadLine();
+        static void UsoDoVar()
+        {
+            // var é usado para o compilador inferir o tipo da variavel que tem a direita na atribuição (tipo implicito de acordo com a atribuição)
+            // nome: inferencia de tipo de variavel
+            var conta = new ContaCorrente(344, 565656);
+            var gerenciador = new GerenciadorBonificacao();
+            var gerenciadores = new List<GerenciadorBonificacao>();
+            var idade = 18;
+            var soma = SomarVarios(18, 23, 25, 51, 55);
+
+            conta.Depositar(3443);
+        }
+
+        static int SomarVarios(params int[] numeros)
+        {
+            int acumulador = 0;
+            foreach (int numero in numeros)
+            {
+                acumulador += numero;
+            }
+            Console.WriteLine($"Soma Total: {acumulador}");
+            return acumulador;
+        }
+
+        static void ListComSort()
+        {
+            var nomes = new List<string>()
+            {
+                "Wellington",
+                "Guilherme",
+                "Luana",
+                "Ana"
+            };
+            nomes.Sort();
+
+            foreach (var nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+
+            var idades = new List<int>();
+            idades.Add(1);
+            idades.Add(5);
+            idades.Add(14);
+            idades.Add(25);
+            idades.Add(38);
+            idades.Add(61);
+            idades.AdicionarVarios(18, 45, 53, 12, 98);
+            idades.AdicionarVarios(99, -1);
+
+            idades.Sort();
+
+
+            for (int i = 0; i < idades.Count; i++)
+            {
+                Console.WriteLine(idades[i]);
+            }
         }
 
     }
